a09f06af8603044179d73e71d8d6e800b3fa29c8
Kbonheur <kurt.bonheure@easypay-group.com>
2022-08-18 15:14:17 +0200

    Added controllers for each type of dependency injection

---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgdi.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+
+class SetterInjectedControllerTest {
+
+	SetterInjectedController controller;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		controller = new SetterInjectedController();
+		controller.setGreetingService(new ConstructorGreetingService());
+	}
+
+	@Test
+	void testGetGreeting() {
+		System.out.println(controller.getGreeting());
+	}
+
+}
